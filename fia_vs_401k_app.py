306e7fe35ea41f2edd2833b9eefdf1151053c62a
cxobeckyklein <57460182+cxobeckyklein@users.noreply.github.com>
2025-06-27 12:16:46 -0700

    Enhanced inputs and frmat fixes

--- fia_vs_401k_app.py
+++ fia_vs_401k_app.py
@@ -65,7 +65,7 @@ def calculate_rmds(balances, ages, tax_rate, inflation_rate):
 # Simulation logic
 def run_simulation(index_choice, start_age, premium, pr_start, pr_end, cap_input, floor, spread, fee, inflation_rate, tax_rate, combined_df):
     st.header("Simulation Results")
-    st.markdown(f"<div style='font-size: 18px; margin-top: -10px; color: grey;'>Index Reference – {index_choice}</div>", unsafe_allow_html=True)
+    st.markdown(f"<div style='font-size: 18px; margin-top: -10px; color: grey;', margin-bottom: +6px>Index Reference – {index_choice}</div>", unsafe_allow_html=True)
 
     selected_data = combined_df[combined_df['Index'] == index_choice][['Year', 'Return']]
     selected_returns = selected_data['Return'].tolist()
